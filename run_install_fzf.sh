b5e7d0f437b957a0428e34cc496cca9ff2126b38
Jeff Scelza <jeffrey.scelza@checkr.com>
2023-01-03 14:23:59 -0700

    Setting up pg and ruby and documentation

--- run_install_fzf.sh
+++ run_install_fzf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "Adding bind key for fzf"
+gecho -e  "--- Start of ${TXT_BOLD}FZF Bindings${TXT_CLEAR} setup ----"
 $(brew --prefix)/opt/fzf/install --key-bindings --completion  --no-bash --no-fish --update-rc
+gecho -e "---- End of ${TXT_BOLD}FZF Bindings${TXT_CLEAR} setup ----"
