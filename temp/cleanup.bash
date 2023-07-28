7ab175dc84f661647628062a65c57e97282d19f7
Jeffrey Scelza <jeffscelza76@gmail.com>
2023-07-28 20:34:15 +0000

    Update 2 files
    - /run_install_asdf.sh.tmpl
    - /temp/cleanup.bash

---
+++ temp/cleanup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+rm -rf .asdfrc .default-python-packages .docker .local bin .config .asdfrc .default-python-packages .docker .local .viminfo
