2d560180f9594cc72da828bc5f88921e5b4e8758
Jeffrey Scelza <jeffscelza76@gmail.com>
2023-07-28 20:51:19 +0000

    Update file cleanup.bash

--- temp/cleanup.bash
+++ temp/cleanup.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-rm -rf .asdfrc .default-python-packages .docker .local bin .config .asdfrc .default-python-packages .docker .local .viminfo
+rm -rf .asdfrc .default-python-packages .docker .local bin .config .asdfrc \
+    .default-python-packages .docker .local .viminfo gitconfig fzf.zsh .gitignore \ 
+    .gnupg .gnupg_pre_2.1 .krew .okteto .spaceshiprc.zsh .ssh .tool-versions .zsh .asdf .fzf.zsh .gitconfig .zshrc
